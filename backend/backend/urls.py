330c9b38f6449389fe9dee89d834fd1d86d3ddc5
JhyunJ1 <JhyunJ1005@gmail.com>
2023-10-18 11:15:22 +0900

    feat

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,11 +19,14 @@ from django.urls import path, include
 from member.views import register, login, id_check
 from htp_test import views
 
+from member.serializer import TokenObtainPairSerializer
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', register),
     path('htp_test/', include('htp_test.urls')),
     path('login/', login),
     path('id_check/', id_check),
+    path('token/', TokenObtainPairSerializer.get_token()),
 ]
 

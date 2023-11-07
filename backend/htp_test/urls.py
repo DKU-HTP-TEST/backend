93d6bc5c83d13abba3a8913a1c1a29773309920d
윤해빈 <ywy2001@naver.com>
2023-11-07 15:27:44 +0900

    feat: ForeignKey user_id

--- backend/htp_test/urls.py
+++ backend/htp_test/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('analyze_img_house/', views.analyze_img_house),
     path('analyze_img_tree/', views.analyze_img_tree),
-    path('analyze_img_person/', views.analyze_img_person)
+    path('analyze_img_person/', views.analyze_img_person),
+    path('result/', views.result),
+    path('del_result/', views.del_result),
 ]
